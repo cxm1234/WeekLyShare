041a4a922690d85c36caff0405deff5174d041c8
chengxiaoming <chengxiaoming@top-stack.com>
2022-10-27 00:04:22 +0800

    part three

--- 10月24日/SwiftUI_Animations/SwiftUI_Animations/Part2_GeometryEffect/Example8.swift
+++ 10月24日/SwiftUI_Animations/SwiftUI_Animations/Part2_GeometryEffect/Example8.swift
@@ -46,6 +46,57 @@ struct Example8: View {
     }
 }
 
+struct LabelView: View {
+    let text: String
+    var offset: CGFloat
+    var pct: CGFloat
+    let backgroundColor: Color
+    
+    var body: some View {
+        Text("The SwiftUI Lab")
+            .font(.headline)
+            .padding(5)
+            .background(RoundedRectangle(cornerRadius: 5).foregroundColor(backgroundColor))
+            .foregroundColor(.black)
+            .modifier(SkewedOffset(offset: offset, pct: pct, goingRight: offset > 0))
+    }
+}
+
+struct SkewedOffset: GeometryEffect {
+    var offset: CGFloat
+    var pct: CGFloat
+    let goingRight: Bool
+    
+    init(offset: CGFloat, pct: CGFloat, goingRight: Bool) {
+        self.offset = offset
+        self.pct = pct
+        self.goingRight = goingRight
+    }
+    
+    var animatableData: AnimatablePair<CGFloat, CGFloat> {
+        get { return AnimatablePair<CGFloat, CGFloat>(offset, pct)}
+        set {
+            offset = newValue.first
+            pct = newValue.second
+        }
+    }
+    
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        var skew: CGFloat
+        
+        if pct < 0.2 {
+            skew = (pct * 5) * 0.5 * (goingRight ? -1 : 1)
+        } else if pct > 0.8 {
+            skew = ((1 - pct) * 5) * 0.5 * (goingRight ? -1 : 1)
+        } else {
+            skew = 0.5 * (goingRight ? -1 : 1)
+        }
+        
+        return ProjectionTransform(CGAffineTransform(1, 0, skew, 1, offset, 0))
+    }
+    
+}
+
 struct Example8_Previews: PreviewProvider {
     static var previews: some View {
         Example8()
